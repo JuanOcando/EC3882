bde7acdd9e9299afe76fc231828133a711042c2b
Orlando Trejo <orlando.trejo@gmail.com>
2018-01-15 12:06:30 -0400

    Upload of the basic examples.
    Beginner tutorial of DEMOQE MC9S08QE128. Up to Serial communications without protocol.

---
+++ ProcessorExpert.c
@@ -0,0 +1,135 @@
+/* ###################################################################
+**     Filename    : ProcessorExpert.c
+**     Project     : ProcessorExpert
+**     Processor   : MC9S08QE128CLK
+**     Version     : Driver 01.12
+**     Compiler    : CodeWarrior HCS08 C Compiler
+**     Date/Time   : 2014-02-18, 15:36, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file ProcessorExpert.c
+** @version 01.12
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup ProcessorExpert_module ProcessorExpert module documentation
+**  @{
+*/         
+/* MODULE ProcessorExpert */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+#include "Bit1.h"
+#include "TI1.h"
+#include "AS1.h"
+#include "AD1.h"
+#include "Cap1.h"
+/* Include shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+unsigned char estado = ESPERAR;
+unsigned char CodError;
+unsigned int Enviados = 2;
+unsigned int error;
+bool primero = FALSE;
+unsigned int periodo;
+
+typedef union{
+unsigned char u8[2];
+unsigned int u16;
+}AMPLITUD;
+
+volatile AMPLITUD iADC;
+unsigned char cTrama[5]={0x00,0x00,0x00,0x00,0xFF};
+unsigned char dTrama[2]={0x00,0x00};
+
+void main(void)
+{
+  /* Write your local variable definition here */
+
+	
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+    
+  /* For example: for(;;) { } */
+
+  for(;;){
+
+  	switch (estado){
+  		
+  		case ESPERAR:
+  			break;
+  			
+  		case MEDIR:
+  			CodError = AD1_Measure(TRUE);
+  			CodError = AD1_GetValue16(&iADC.u16);
+  			estado = ENVIAR;
+  			break;
+  	
+  		case ENVIAR:
+  			if(iADC.u8[0] != 0xFF){
+  				cTrama[1] = iADC.u8[0];
+  				cTrama[3] = 0x00;	
+  			}else{
+  				cTrama[1] = 0xFE;
+  				cTrama[3] = 0x00;
+  			}
+  			
+  			if(iADC.u8[1] != 0xFF){
+  				cTrama[2] = iADC.u8[1];				
+  			}else{
+  				cTrama[2] = 0xFE;
+  				cTrama[3] = cTrama[3] | 0x0A;
+  			}
+  	
+  			dTrama[0]=(periodo >> 8) & (0xFF);
+  			dTrama[1]=(periodo) & (0xFF);
+  			
+  			CodError = AS1_SendBlock(dTrama,2,&Enviados);
+  			estado = ESPERAR;
+  			
+  			break;
+  			
+  		default:
+  			break;
+  	
+  	}
+  }
+  
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END ProcessorExpert */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.08]
+**     for the Freescale HCS08 series of microcontrollers.
+**
+** ###################################################################
+*/
